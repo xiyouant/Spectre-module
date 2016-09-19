fe70017913dd53522a61e5b626f243a3271109e3
leozhang2018 <leozhang2018@gmail.com>
2016-09-19 15:31:11 +0800

    rm old files

--- src/data/slideData.js
+++
@@ -1,5 +0,0 @@
-var imgData =[
-				{title:'',href:'#',imgsrc:'http://7xlqb6.com1.z0.glb.clouddn.com/1.jpg',alt:'jpg1'},
-				{title:'',href:'#',imgsrc:'http://7xlqb6.com1.z0.glb.clouddn.com/2.jpeg',alt:'jpg2'},
-				{title:'',href:'#',imgsrc:'http://7xlqb6.com1.z0.glb.clouddn.com/3.jpg',alt:'jpg3'}
-			]
